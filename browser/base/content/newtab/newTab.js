c8c6a496742e2a31c917a47b35e449a7fbbf7b9a
Marina Samuel <msamuel@mozilla.com>
2015-08-26 17:37:56 -0400

    Bug 1183206 - Make about:newtab independent of Firefox

--- browser/base/content/newtab/newTab.js
+++ browser/base/content/newtab/newTab.js
@@ -1,62 +1,101 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
+/*globals Components, sendAsyncMessage, addMessageListener*/
 
 "use strict";
+(function() {
+  const {
+    utils: Cu
+  } = Components;
+  const TRUSTED_ORIGIN = "https://mozilla.github.io";
+  const {
+    XPCOMUtils
+  } = Cu.import("resource://gre/modules/XPCOMUtils.jsm", {});
+  const imports = {};
+  XPCOMUtils.defineLazyModuleGetter(imports, "PrivateBrowsingUtils",
+    "resource://gre/modules/PrivateBrowsingUtils.jsm");
+  XPCOMUtils.defineLazyModuleGetter(imports, "Services",
+    "resource://gre/modules/Services.jsm");
 
-let Cu = Components.utils;
-let Ci = Components.interfaces;
+  let iframe;
 
-Cu.import("resource://gre/modules/XPCOMUtils.jsm");
-Cu.import("resource://gre/modules/Services.jsm");
-
-XPCOMUtils.defineLazyModuleGetter(this, "Rect",
-  "resource://gre/modules/Geometry.jsm");
-XPCOMUtils.defineLazyModuleGetter(this, "PrivateBrowsingUtils",
-  "resource://gre/modules/PrivateBrowsingUtils.jsm");
-XPCOMUtils.defineLazyModuleGetter(this, "UpdateChannel",
-  "resource://gre/modules/UpdateChannel.jsm");
-
-
-XPCOMUtils.defineLazyGetter(this, "gStringBundle", function() {
-  return Services.strings.
-    createBundle("chrome://browser/locale/newTab.properties");
-});
-
-function newTabString(name, args) {
-  let stringName = "newtab." + name;
-  if (!args) {
-    return gStringBundle.GetStringFromName(stringName);
+  function handleCommand(command, data) {
+    let commandHandled = true;
+    switch (command) {
+    case "NewTab:UpdateTelemetryProbe":
+      imports.Services.telemetry.getHistogramById(data.probe).add(data.value);
+      break;
+    case "NewTab:Register":
+      registerEvent(data.type);
+      break;
+    case "NewTab:GetInitialState":
+      getInitialState();
+      break;
+    default:
+      commandHandled = false;
+    }
+    return commandHandled;
   }
-  return gStringBundle.formatStringFromName(stringName, args, args.length);
-}
 
-function inPrivateBrowsingMode() {
-  return PrivateBrowsingUtils.isContentWindowPrivate(window);
-}
+  function initRemotePage() {
+    // Messages that the iframe sends the browser will be passed onto
+    // the privileged parent process
+    let iframe = getIframe();
+    let loadHandler = () => {
+      iframe.removeEventListener("load", loadHandler);
+      iframe.contentDocument.addEventListener("NewTabCommand", (e) => {
+        let handled = handleCommand(e.detail.command, e.detail.data);
+        if (!handled) {
+          sendAsyncMessage(e.detail.command, e.detail.data);
+        }
+      });
+      registerEvent("NewTab:Observe");
+      let ev = new CustomEvent("NewTabCommandReady");
+      iframe.contentDocument.dispatchEvent(ev);
+    };
+    // Check if iframe already fired its onload event
+    if (iframe.contentDocument.readyState === "complete") {
+      loadHandler();
+      return;
+    }
+    iframe.addEventListener("load", loadHandler);
+  }
 
-const HTML_NAMESPACE = "http://www.w3.org/1999/xhtml";
-const XUL_NAMESPACE = "http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul";
+  function registerEvent(event) {
+    // Messages that the privileged parent process sends will be passed
+    // onto the iframe
+    addMessageListener(event, (message) => {
+      let iframe = getIframe();
+      iframe.contentWindow.postMessage(message, TRUSTED_ORIGIN);
+    });
+  }
 
-const TILES_EXPLAIN_LINK = "https://support.mozilla.org/kb/how-do-tiles-work-firefox";
-const TILES_INTRO_LINK = "https://www.mozilla.org/firefox/tiles/";
-const TILES_PRIVACY_LINK = "https://www.mozilla.org/privacy/";
+  function getInitialState() {
+    let prefs = imports.Services.prefs;
+    let isPrivate = imports.PrivateBrowsingUtils.isContentWindowPrivate(window);
+    let state = {
+      enabled: prefs.getBoolPref("browser.newtabpage.enabled"),
+      enhanced: prefs.getBoolPref("browser.newtabpage.enhanced"),
+      rows: prefs.getIntPref("browser.newtabpage.rows"),
+      columns: prefs.getIntPref("browser.newtabpage.columns"),
+      introShown: prefs.getBoolPref("browser.newtabpage.introShown"),
+      privateBrowsingMode: isPrivate
+    };
+    let iframe = getIframe();
+    iframe.contentWindow.postMessage({
+      name: "NewTab:State",
+      data: state
+    }, TRUSTED_ORIGIN);
+  }
 
-#include transformations.js
-#include page.js
-#include grid.js
-#include cells.js
-#include sites.js
-#include drag.js
-#include dragDataHelper.js
-#include drop.js
-#include dropTargetShim.js
-#include dropPreview.js
-#include updater.js
-#include undo.js
-#include search.js
-#include customize.js
-#include intro.js
+  function getIframe() {
+    if (!iframe) {
+      iframe = document.getElementById("remotedoc");
+    }
+    return iframe;
+  }
 
-// Everything is loaded. Initialize the New Tab Page.
-gPage.init();
+  // Everything is loaded. Initialize the New Tab Page.
+  initRemotePage();
+}());
