613ba881d5d11b8cc52a32605b3172e7fc02f201
Olivier Yiptong <olivier@olivieryiptong.com>
2015-10-15 13:27:23 -0400

    Bug 1196762 - Implement a pref provider for remote newtab 1/2 r?Mardak

---
+++ browser/components/newtab/tests/xpcshell/test_NewTabPrefsProvider.js
@@ -0,0 +1,27 @@
+"use strict";
+
+/* global XPCOMUtils */
+/* global ok */
+
+const Cu = Components.utils;
+Cu.import("resource://gre/modules/XPCOMUtils.jsm");
+Cu.import("resource://gre/modules/Services.jsm");
+
+XPCOMUtils.defineLazyModuleGetter(this, "NewTabPrefsProvider",
+    "resource:///modules/NewTabPrefsProvider.jsm");
+
+function run_test() {
+  run_next_test();
+}
+
+add_task(function* test_observe() {
+  Services.prefs.setBoolPref("browser.newtabpage.enabled", false);
+  NewTabPrefsProvider.prefs.startTracking();
+  let promise = new Promise(resolve => {
+    NewTabPrefsProvider.prefs.once("browser.newtabpage.enabled", resolve);
+  });
+  Services.prefs.setBoolPref("browser.newtabpage.enabled", true);
+  let data = yield promise;
+  ok(data, "pref emitter triggers");
+  NewTabPrefsProvider.prefs.stopTracking();
+});
