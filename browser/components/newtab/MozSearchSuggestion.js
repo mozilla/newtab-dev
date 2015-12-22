d26fcc214d6649c15c78c3d3215457a00345efb4
Marcos Caceres <marcos@marcosc.com>
2015-12-22 15:03:05 +1100

    Cleanup of MozSearchSuggestions.js

--- browser/components/newtab/MozSearchSuggestion.js
+++ browser/components/newtab/MozSearchSuggestion.js
@@ -12,24 +12,7 @@ const {
 Cu.import("resource://gre/modules/XPCOMUtils.jsm");
 Cu.importGlobalProperties(["URL"]);
 
-
 function MozSearchSuggestion() {}
-
-function out(msg) {
-  dump(`
-=============&&&&&&&&&&&&&============
-${msg}
-
-`);
-}
-
-function dumpObj(r) {
-  dump(`\n ================ DUMPING object ${r} =========\n`);
-  for (var i in r) {
-    out(`${i} -> ${r[i]} (type: ${typeof r[i]})`);
-  }
-  return r;
-}
 
 MozSearchSuggestion.prototype = {
   classDescription: "Implementation of MozSearchSuggestion",
@@ -42,25 +25,28 @@ MozSearchSuggestion.prototype = {
   _searchString: "",
   _formHistory: null,
   _remote: null,
+
   get engineName() {
     return this._engineName;
   },
+
   get searchString() {
     return this._searchString;
   },
+
   get formHistory() {
     return this._formHistory;
   },
+
   get remote() {
     return this._remote;
   },
+
   init(contentWindow) {
     this._win = contentWindow;
   },
+
   __init(suggestions) {
-    dumpObj(suggestions);
-    dump(suggestions.formHistory);
-    dump(suggestions);
     this._engineName = suggestions.engineName;
     this._searchString = suggestions.searchString;
     this._formHistory = this._fillSafeArray(suggestions.formHistory.split(","));
@@ -71,7 +57,8 @@ MozSearchSuggestion.prototype = {
     let safeArray = new this._win.Array();
     safeArray.push(...iterable);
     return safeArray;
-  }
+  },
+
 };
 
 var components = [MozSearchSuggestion];
