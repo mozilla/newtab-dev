02443b23491dd55f5360c80ba21e9ff35b8d9bd9
Ursula Sarracini <ursulasarracini@gmail.com>
2016-02-18 13:34:26 -0500

    starting tests for search provider

---
+++ browser/components/newtab/tests/browser/browser_newtabsearchprovider.js
@@ -0,0 +1,80 @@
+"use strict";
+
+/* global XPCOMUtils, NewTabSearchProvider, run_next_test, ok, is, Services */
+/* exported run_test */
+/* jscs:disable requireCamelCaseOrUpperCaseIdentifiers */
+
+const Cu = Components.utils;
+Cu.import("resource://gre/modules/XPCOMUtils.jsm");
+Cu.import("resource://gre/modules/Services.jsm");
+
+// create test engine called MozSearch
+Services.search.addEngineWithDetails("TestSearch1", "", "", "", "GET",
+  "http://example.com/?q={searchTerms}");
+Services.search.defaultEngine = Services.search.getEngineByName("TestSearch1");
+Services.search.addEngineWithDetails("TestSearch2", "", "", "", "GET",
+  "http://example.com/?q={searchTerms}");
+
+XPCOMUtils.defineLazyModuleGetter(this, "NewTabSearchProvider",
+    "resource:///modules/NewTabSearchProvider.jsm");
+
+function run_test() {
+  run_next_test();
+}
+
+function hasProp(obj) {
+  return function(aProp) {
+    ok(obj.hasOwnProperty(aProp), `expect to have property ${aProp}`);
+  };
+}
+
+add_task(function* test_state() {
+  // get initial state of search and check it has correct properties
+  let state = yield NewTabSearchProvider.search.state;
+  let stateProps = hasProp(state);
+  ["engines", "currentEngine"].forEach(stateProps);
+
+  // check that the current engine is correct and has correct properties
+  let {currentEngine} = state;
+  is(currentEngine.name, Services.search.currentEngine.name, "Current engine has been correctly set");
+  var engineProps = hasProp(currentEngine);
+  ["name", "placeholder", "iconBuffer"].forEach(engineProps);
+});
+
+/*add_task(function* test_search() {
+  let searchData = {
+    engineName: Services.search.currentEngine.name,
+    searchString: "test",
+    healthReportKey: "1",
+    searchPurpose: "d"
+  };
+
+  // perform a search & add to form history
+
+  // check suggestions have good properties
+
+  // remove it from the form history
+
+});*/
+
+add_task(function* test_observe() {
+  // test that the event emitter is working by setting a new current engine "Google"
+  let engineName = "TestSearch2";
+
+  NewTabSearchProvider.search.init();
+  let promise = new Promise(resolve => {
+    NewTabSearchProvider.search.once("browser-search-engine-modified", (name, data) => { // jshint ignore:line
+      resolve([name, data.name]);
+    });
+  });
+
+  // set a new current engine
+  Services.search.currentEngine = Services.search.getEngineByName(engineName);
+  let expectedEngineName = Services.search.currentEngine.name;
+
+  // emitter should fire and return the new engine
+  //debugger;
+  let [name, actualEngineName] = yield promise;
+  is(expectedEngineName, actualEngineName, `emitter set the correct engine ${expectedEngineName}`);
+  NewTabSearchProvider.search.uninit();
+});
